7f67df2811547263aa953902408e543bbdb86246
Luke100000 <jonas.peche@aon.at>
2023-09-20 15:26:41 +0200

    lets let registering crash on adding invalid items

--- common/src/main/java/immersive_melodies/Items.java
+++ common/src/main/java/immersive_melodies/Items.java
@@ -39,25 +39,23 @@ public interface Items {
 
     /**
      * Open method to create custom items, for addons
+     *
      * @param namespace Your addon's namespace
-     * @param name Your addon's item name
-     * @param animator Your item's animator. Allows to define how the entity
-     *                 model should be animated when playing the instrument.
-     * @param sustain Determines the instrument's note sustain capacity.
-     *                Defines how many ticks can a note hold for the longest
-     *                with your custom instrument.
-     * @param hOffset Determines the horizontal offset from the player's location
-     *                of the position at which a note particle should be displayed.
-     * @param vOffset Determines the vertical offset from the player's location
-     *                of the position at which a note particle should be displayed.
+     * @param name      Your addon's item name
+     * @param animator  Your item's animator. Allows to define how the entity
+     *                  model should be animated when playing the instrument.
+     * @param sustain   Determines the instrument's note sustain capacity.
+     *                  Defines how many ticks can a note hold for the longest
+     *                  with your custom instrument.
+     * @param hOffset   Determines the horizontal offset from the player's location
+     *                  of the position at which a note particle should be displayed.
+     * @param vOffset   Determines the vertical offset from the player's location
+     *                  of the position at which a note particle should be displayed.
      * @return The registered item's provider.
      */
     static @Nullable Supplier<Item> register(@NotNull String namespace, @NotNull String name, Animator animator,
                                              long sustain, float hOffset, float vOffset) {
-        Identifier identifier = Identifier.of(namespace, name);
-        if (identifier == null) {
-            return null;
-        }
+        Identifier identifier = new Identifier(namespace, name);
         Supplier<Item> supplier = register(namespace, name, sustain, hOffset, vOffset);
         ItemAnimators.register(identifier, animator);
         return supplier;
@@ -66,22 +64,20 @@ public interface Items {
     /**
      * Open method to create custom items, for addons.
      * If using this method, make sure to also register an {@link Animator} for your item.
+     *
      * @param namespace Your addon's namespace
-     * @param name Your addon's item name
-     * @param sustain Your item's animator. Allows to define how the entity
-     *                model should be animated when playing the instrument.
-     * @param hOffset Determines the horizontal offset from the player's location
-     *                of the position at which a note particle should be displayed.
-     * @param vOffset Determines the vertical offset from the player's location
-     *                of the position at which a note particle should be displayed.
+     * @param name      Your addon's item name
+     * @param sustain   Your item's animator. Allows to define how the entity
+     *                  model should be animated when playing the instrument.
+     * @param hOffset   Determines the horizontal offset from the player's location
+     *                  of the position at which a note particle should be displayed.
+     * @param vOffset   Determines the vertical offset from the player's location
+     *                  of the position at which a note particle should be displayed.
      * @return The registered item's provider.
      */
     static @Nullable Supplier<Item> register(@NotNull String namespace, @NotNull String name,
                                              long sustain, float hOffset, float vOffset) {
-        Identifier identifier = Identifier.of(namespace, name);
-        if (identifier == null) {
-            return null;
-        }
+        Identifier identifier = new Identifier(namespace, name);
         Sounds.Instrument instrument = new Sounds.Instrument(namespace, name);
         Supplier<Item> itemSupplier = () -> new InstrumentItem(baseProps(), instrument, sustain, hOffset, vOffset);
         Supplier<Item> supplier = Registration.register(Registries.ITEM, identifier, itemSupplier);
